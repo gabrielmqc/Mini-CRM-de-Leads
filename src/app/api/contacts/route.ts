369ce5c25bc962dd3286e8e0de0e81c1629e1e27
gabrielmqc <gabrielmazzo2014@gmail.com>
2026-02-10 10:15:04 -0300

    feat: create and list contacts

--- src/app/api/contacts/route.ts
+++ src/app/api/contacts/route.ts
@@ -1,12 +1,34 @@
 import { handle } from "hono/vercel";
 import { createHonoApp } from "../_global/hono";
+import { InMemoryContactRepository } from "@/src/infrastructure/repositories/InMemoryContactRepository";
+import { createContactSchema } from "@/src/validation/contact.schema";
+import { CreateContact } from "@/src/application/use-cases/contacts/CreateContact";
+import { GetContacts } from "@/src/application/use-cases/contacts/GetContacts";
 
 const app = createHonoApp();
 
-app.get("/contacts", (c) => {
-  return c.json({
-    message: "Hello from Contatcs!",
-  });
+const contactRepo = new InMemoryContactRepository();
+
+app.post("/contacts", async (c) => {
+  const body = await c.req.json();
+
+  const parsed = createContactSchema.safeParse(body);
+  if (!parsed.success) {
+    return c.json({ errors: parsed.error.format() }, 400);
+  }
+
+  const useCase = new CreateContact(contactRepo);
+  const contact = await useCase.execute(parsed.data);
+
+  return c.json(contact, 201);
+});
+
+app.get("/contacts", async (c) => {
+  const search = c.req.query("search");
+  const useCase = new GetContacts(contactRepo);
+  const contacts = await useCase.execute({ query: search });
+  return c.json(contacts);
 });
 
 export const GET = handle(app);
+export const POST = handle(app);
---
+++ src/application/use-cases/contacts/GetContacts.ts
@@ -0,0 +1,22 @@
+import { IContact } from "../../gateways/IContact";
+import { Contact } from "@/src/domain/entities/Contact";
+
+interface Input {
+  query?: string;
+}
+
+export class GetContacts {
+  constructor(private repo: IContact) {}
+
+  async execute(input: Input): Promise<Contact[]> {
+    console.log("Executing GetContacts", input.query);
+    if (!input.query) {
+      return this.repo.findAll();
+    }
+    if (input.query.includes("@")) {
+      return this.repo.findByEmail(input.query);
+    } else {
+      return this.repo.findByName(input.query);
+    }
+  }
+}
